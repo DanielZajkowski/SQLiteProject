0304c95c7f23b9342a1a4ae5dc56928fae87df83
DanielZajkowski <zajkowskidaniel@gmail.com>
2021-06-18 15:27:25 +0200

    Adding connection and exceptions

--- main.cpp
+++ main.cpp
@@ -1,35 +1,14 @@
-#include "sqlite3.h"
 #include <stdio.h>
-#include "Handle.h"
-
-#ifdef _DEBUG
-#define VERIFY ASSERT
-#define VERIFY_(result, expression) ASSERT(result == expression)
-#else
-#define VERIFY(expression) (expression)
-#define VERIFY_(result, expression) (expression)
-#endif // _DEBUG
-
-struct ConnectionHandleTraits : HandleTraits<sqlite3*>
-{
-	static void Close(Type value) noexcept
-	{
-		VERIFY_(SQLITE_OK, sqlite3_close(value));
-	}
-};
-
-using ConnectionHandle = Handle<ConnectionHandleTraits>;
+#include "SQLite.h"
 
 int main()
 {
-	//sqlite3* connection = nullptr;
-	ConnectionHandle connection;
-
-	int result = sqlite3_open(":memory:", connection.Set());
-
-	if (SQLITE_OK != result)
+	try
 	{
-		printf("%s\n", sqlite3_errmsg(connection.Get()));
-		return result;
+		Connection connection = Connection::Memory();
+	}
+	catch (Exception const& e)
+	{
+		printf("%s (%d)\n", e.Message.c_str(), e.Result);
 	}
 }
